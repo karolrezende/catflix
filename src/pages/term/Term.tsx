bb174d1a7e4f435dd4a614c6c3ce9f7cecd6c0b2
karolrezende <karolrds01@gmail.com>
2023-05-12 10:39:55 -0400

    catsponssabilidade

--- src/pages/term/Term.tsx
+++ src/pages/term/Term.tsx
@@ -2,6 +2,7 @@ import superCat from './content/supercat.png'
 import catWalk from './content/catwalk.png'
 import sorryCat from './content/sorricat.png'
 import styles from './styles.module.scss'
+import { Link } from 'react-router-dom'
 export default function Term() {
   return (
     <div className={styles.div}>
@@ -10,9 +11,10 @@ export default function Term() {
         <div className={styles.div_div_border}>
           <h1>TERMO DE CATSPONSABILIDADE</h1>
           <p>
-          Ao aceitar nosso termo de responsabilidade você concorda em estar apto a <br></br> amarincodicionalmente e irrevogávelmente todos os animais presentes no  <br></br>planeta terra (especialmente os gatos). Irá estar apto cuida-los, acaricia-los e <br></br> ama-los em tempo integral, dedicando toda sua mera existencia a sua <br></br>plenitude. Sempre que possivel irá acolhe-los, alimenta-los e afaga-los, caso<br></br> haja necessidade também irá tratar de suas feridas e curar seus machucados.
+          Ao aceitar nosso termo de responsabilidade você concorda em estar apto a <br></br> amar incodicionalmente e irrevogávelmente todos os animais presentes no  <br></br>planeta terra (especialmente os gatos). Irá estar apto cuida-los, acaricia-los e <br></br> ama-los em tempo integral, dedicando toda sua mera existencia a sua <br></br>plenitude. Sempre que possivel irá acolhe-los, alimenta-los e afaga-los, caso<br></br> haja necessidade também irá tratar de suas feridas e curar seus machucados.
           </p>
           <img src={sorryCat} alt="Gatinho sorrycat" />
+          <Link to='/cadastro' className={styles.catAccept}>Aceitar</Link>
         </div>
       </div>
       <img src={catWalk} alt="Gatinho de fundo" className={styles.div_cat2} />
